159d02d2ae278945ccc5bb9f232e72b11fa7cb3d
John Pendergrass <john.pendergrass@me.com>
2020-05-06 11:17:22 -0500

    created schema and seeds files

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+
+CREATE DATABASE burgers_db;
+USE cat_db;
+
+CREATE TABLE burgers
+(
+	id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	
+);
